ce055cd4c8920964daa199b13fabc6ca1157291c
Low Siow Meng <siowmeng.low@gmail.com>
2017-02-19 12:19:36 +0000

    Amended to KMeans Cluster 300 Argentina Players instead of 500 English

--- Assignment4/SM-Assignment4Group.py
+++ Assignment4/SM-Assignment4Group.py
@@ -19,10 +19,12 @@ attributes=['Crossing','Finishing','Heading Accuracy',
  'Standing Tackle','Sliding Tackle','GK Diving',
  'GK Handling','GK Kicking','GK Positioning','GK Reflexes']
  
-links=[]   #get all english players
+links=[]   # links containing the player pages to get
 # Q3: Download first 500
-for offset in ['0','100','200', '300', '400']:
-    page=requests.get('http://sofifa.com/players?na=14&offset='+offset) # 14 is for England team
+#for offset in ['0','100','200', '300', '400']:
+for offset in ['0','100','200']:
+    #page=requests.get('http://sofifa.com/players?na=14&offset='+offset) # 14 is for England team
+    page=requests.get('http://sofifa.com/players?na=52&offset='+offset) # 52 is for Argentina team
     soup=BeautifulSoup(page.content,'html.parser')
     for link in soup.find_all('a'):
         links.append(link.get('href'))
@@ -51,29 +53,42 @@ for j, link in enumerate(links):
     rows.append(row) # Append all the information of this player
     print(row[1]) # Print Player Name
 df=pd.DataFrame(rows,columns=['link','name','position']+attributes) # Initialise Panda Dataframe
-df.to_csv('EnglishPlayers.csv',index=False) # Write to EnglishPlayers.csv
+#df.to_csv('EnglishPlayers.csv',index=False) # Write to EnglishPlayers.csv
+df.to_csv('ArgentinaPlayers.csv',index=False) # Write to ArgentinaPlayers.csv
 
 # Q4
 
 # Number of clusters
 numClusters = 5
          
-engPlayers = pd.read_csv('EnglishPlayers.csv')
-engAttrs = engPlayers.iloc[ : , 3: ].as_matrix() # Only use the attributes for clustering
-kmeans = KMeans(init='k-means++', n_clusters = numClusters, n_init = 10, random_state = 99).fit(engAttrs)
+#engPlayers = pd.read_csv('EnglishPlayers.csv')
+argPlayers = pd.read_csv('ArgentinaPlayers.csv')
+#engAttrs = engPlayers.iloc[ : , 3: ].as_matrix() # Only use the attributes for clustering
+argAttrs = argPlayers.iloc[ : , 3: ].as_matrix() # Only use the attributes for clustering
+#kmeans = KMeans(init='k-means++', n_clusters = numClusters, n_init = 10, random_state = 99).fit(engAttrs)
+kmeans = KMeans(init='k-means++', n_clusters = numClusters, n_init = 10, random_state = 99).fit(argAttrs)
 
 # Q5
 
 # From the five Excel files, we can see:
 # See the following link for football positons, https://en.wikipedia.org/wiki/Association_football_positions
+# For 500 English Players
 # Cluster 0 = Wide Range of Defenders (Left-Backs, Centre-Backs, Right-Backs)
 # Cluster 1 = Mainly Strikers and Attacking Midfielders
 # Cluster 2 = Mainly Goalkeepers
 # Cluster 3 = Mainly Defensive Midfielders
 # Cluster 4 = Mainly Centre-Backs (Defenders)
+# For 400 Argentina Players
+# Cluster 0 = Mainly Strikers
+# Cluster 1 = Mainly Centre-Backs (Defenders)
+# Cluster 2 = All Goalkeepers
+# Cluster 3 = Mainly Defenders (Left-Backs, Right-Backs) and Defensive Midfielders
+# Cluster 4 = Mainly Attacking Midfielders and some strikers
 for i in range(numClusters):
-    filename = 'EnglishCluster' + str(i) + '.csv'
-    engPlayers.iloc[kmeans.labels_ == i, 1:3].to_csv(filename)
+    #filename = 'EnglishCluster' + str(i) + '.csv'
+    filename = 'ArgentinaCluster' + str(i) + '.csv'
+    #engPlayers.iloc[kmeans.labels_ == i, 1:3].to_csv(filename)
+    argPlayers.iloc[kmeans.labels_ == i, 1:3].to_csv(filename)
 
 # Q6
 newPlayerAttr = np.array([45, 40, 35, 45, 60, 40, 15]) # Attribute of new player
@@ -92,5 +107,7 @@ centroidsSelected = centroids[ : , selectedAttrs]
 # Calculate the distance to each of the 5 centroids, using only these selected attributes
 distCentroids = np.linalg.norm(newPlayerAttr - centroidsSelected, axis = 1)
 
-np.argmin(distCentroids) # Index = 4, so this new player is closest to the 4th cluster
+# (500 English Players) closest cluster = Cluster with index 4, centre-backs (defenders) cluster
+# (300 Argentina Players) closest cluster = Cluster with index 1, centre-backs (defenders) cluster
+np.argmin(distCentroids)
          
